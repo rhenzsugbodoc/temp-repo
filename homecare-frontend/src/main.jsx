031b5f87d1f0ddd237f11d75d9642bca7fc200d8
Rhenz Largo <rhenzlargo80@gmail.com>
2025-09-26 00:07:44 +0800

    created react app within intern-rhenz-project

---
+++ homecare-frontend/src/main.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Landing from './pages/Landing/Landing.jsx'
+// import Login from './pages/Login/Login.jsx'
+// import Register from './pages/Register/Register.jsx'
+// import Dashboard from './pages/Dashboard/Dashboard.jsx'
+// import Schedule from './pages/Schedule/Schedule.jsx'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+
+
+
+
+
+const router = createBrowserRouter([
+
+{
+  path: '/',
+  element: <Landing/>
+},
+// {
+//   path: '/register',
+//   element: <Register/>
+// },
+// {
+//   path: '/login',
+//   element: <Login/>
+// },
+// {
+//   path: '/dashboard',
+//   element: <Dashboard/>
+// },
+// {
+//   path: '/schedule',
+//   element: <Schedule/>
+// }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
+)
